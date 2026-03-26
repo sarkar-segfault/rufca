398ffc34a5e1c28eb87d414d3ceba21ee3d7effc
Adhrit Sarkar <adhritsarkar@proton.me>
2026-03-26 13:49:11 +0530

    i fvcking hate my life

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ mod new;
 mod opts;
 
 use clap::Parser;
+use colored::Colorize;
 
 fn main() {
     let opts = opts::Options::parse();
 
     match opts.cmd {
-        opts::Command::New { file } => println!("new {file:?}"),
+        opts::Command::New { file } => new::new(),
         opts::Command::Run { file } => println!("run {file:?}"),
     }
 }
--- src/new.rs
+++ src/new.rs
@@ -1,1 +1,28 @@
+use colored::Colorize;
+use std::io::{Write, stdin, stdout};
 
+fn prompt(text: &str) -> Option<String> {
+    print!("{}", text.blue());
+    stdout()
+        .flush()
+        .unwrap_or_else(|e| println!("{}", e.to_string().red()));
+
+    let mut buf = String::new();
+    let n = stdin().read_line(&mut buf).unwrap_or(0);
+
+    if n == 0 {
+        None
+    } else {
+        Some(buf.trim_end().trim_start().to_string())
+    }
+}
+
+pub fn new() {
+    println!(
+        "{}",
+        "At any optional input, you may send EOF to signal nothing.".bright_black()
+    );
+
+    let name = prompt("(Optional) What is the name of the new flashcard set? ");
+    let author = prompt("(Optional) Who is the author of the new flashcard set? ");
+}
