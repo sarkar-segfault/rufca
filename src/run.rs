5aa45d2d1d1fdb973a0f4fbef064276988b597da
Adhrit Sarkar <adhritsarkar@proton.me>
2026-03-27 11:44:53 +0530

    added basic processing for "rufca run <file>"

--- src/run.rs
+++ src/run.rs
@@ -1,3 +1,28 @@
-pub fn run(_file: String) {
-    todo!()
+use crate::models::*;
+use colored::Colorize;
+use serde_json::from_str;
+use std::fs::read_to_string;
+
+pub fn run(file: String) {
+    let fcs: FlashCardSet = from_str(&read_to_string(file).expect("Failed to read file"))
+        .expect("Failed to parse JSON");
+
+    match fcs.name {
+        Some(n) => println!("{} {}", "Welcome to".bright_black(), n.green()),
+        None => (),
+    }
+
+    match fcs.author {
+        Some(a) => println!("{} {}", "created by".bright_black(), a.green()),
+        None => (),
+    }
+
+    match fcs.total {
+        0 => (),
+        _ => println!(
+            "{} {}",
+            "with total marks".bright_black(),
+            fcs.total.to_string().green()
+        ),
+    }
 }
